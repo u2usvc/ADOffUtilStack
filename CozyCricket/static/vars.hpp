8ae27d93549ea0f770bf5a1742672dc4c6a5463d
u2usvc <kwstisulvia@hotmail.com>
2025-11-19 17:52:38 +0300

    move vars.http to a separate directory

---
+++ CozyCricket/static/vars.hpp
@@ -0,0 +1,2 @@
+#define BYTECODE_URI "http://192.168.88.250:9595/build/tmp/agent.bin.enc"
+#define KEY "dsfjdshjfdsfdsfdsfydsgfydsgfuygdsifgdsiugfidsgfigdsu"
--- CozyCricket/vars.cpp
+++
@@ -1,3 +0,0 @@
-// CHANGEME
-#define INSTRUCTIONS_URI "http://192.168.88.250:9595/build/tmp/test.txt.bin"
-#define KEY "dsfjdshjfdsfdsfdsfydsgfydsgfuygdsifgdsiugfidsgfigdsu"
